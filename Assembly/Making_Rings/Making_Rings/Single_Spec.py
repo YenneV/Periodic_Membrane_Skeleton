db068aeba7f55043c66a786838bc088a2fe213d5
YenneV <xhelenmariex@yahoo.com>
2018-06-20 22:14:08 +0100

    Tidied and Tested
    Combined scatter and histogram code into one file.

--- Assembly/Making_Rings/Making_Rings/Single_Spec.py
+++ Assembly/Making_Rings/Making_Rings/Single_Spec.py
@@ -5,46 +5,8 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Function that does a full walk on 1 particle for a given no. of steps
-def rand_walk(steps, initial, xrange, yrange):
-    # Array to store coordinates for each timestep
-    walk_path = np.zeros([len(steps),2], dtype=int)
-
-    # Taking steps
-    for i in steps:
-        print('Current iteration = ')
-        print(i)
-        grid_status = np.zeros([len(xrange),len(yrange)], dtype=int) #grid of zeros to store occupation status
-        if i == 0: # Initial position set based on initial values given when calling function
-            x = initial[0]
-            y = initial[1]
-        else: #Calculating next set of x,y coordinates for time step i > 0 using random numbers
-            step = randint(0,3)
-            if step == 0:
-                x += 1
-            elif step == 1:
-                x -= 1
-            elif step == 2:
-                y += 1
-            elif step == 3:
-                y -= 1
-            else: #Not really necessary but just to make sure rand is drawn in range
-                print('Step error - out of range')
-                break
-        #grid_status[x,y] = 1 #Setting status of x,y position as occupied
-        walk_path[i,] = [x,y] #Recording new coordinates in array
-        #print('current location')
-        #print(walk_path[i,])
-        if walk_path[i,0] not in xrange or walk_path[i,1] not in yrange: #Checking that particle is within cell
-            x = walk_path[i-1,0] #if outside cell, set coordinates to previous - i.e. particle doesn't move
-            y = walk_path[i-1,1]
-            walk_path[i,] = walk_path[i-1,] #recording edited coordinates in array
-            print('step rejected - out of range')
-            #print(walk_path[i,])
-
-    return walk_path
-
-# Function that does a single step given an initial position
+# Function 'step' does a single step given an initial position and a set of allowed coordinates
+# The new coordinates are returned to the calling function
 def step(initial, xrange, yrange):
     x = initial[0]
     y = initial[1]
@@ -72,13 +34,9 @@ def step(initial, xrange, yrange):
         return final
 
 
-# Parameters for the allowed set of coordinates, number of timesteps etc
-xcoords = list(range(100))
-ycoords = list(range(100))
-timesteps = list(range(1,101)) # Number of iterations, integers from 1 to 100 inclusive
-initial_position = [50,50] #Initial x,y coordinates of spectrin
-
-# Next I want to make a function that creates a random walk of a given number of steps
+# Function 'walk' takes a starting position and a given number of timesteps, and creates a random walk 
+# of the specified step length by calling the step function.  A vector of coordinates per time step
+# is returned to the calling function
 
 def walk(initial_position, timesteps):
     location = np.zeros([len(timesteps) + 1,2], dtype = int)
@@ -91,8 +49,8 @@ def walk(initial_position, timesteps):
 
     return location
 
-# I now want to make a function that calls, walk, generates a random walk, and calculates the
-# end to end distance travelled by the particle during the walk, then returns it 
+# Function 'distance' generates a random walk by calling 'walk', then calculates the magnitude of the
+# distance walked and returns it to the calling function
 
 def distance():
     route = walk(initial_position, timesteps)
@@ -105,38 +63,37 @@ def distance():
     distance = math.sqrt((delta_x)**2 + (delta_y)**2)
     return distance
 
-# And finally a function that runs distance loads of times and gives me all of the values
-# so that I can plot a histogram
+# Function 'travels' records distances walked by calling 'distance', and returns a vector with the 
+# distance travelled in a given number of random walks - for statistical analysis
 
-def travels():
-    iterations = list(range(250000))
+def travels(iterations):
     walks = np.zeros(len(iterations), dtype = float)
-
     for i in iterations:
         walks[i] = distance()
 
     return walks
 
-stuff = travels()
-print(stuff)
+# INPUT PARAMETERS
+xcoords = list(range(1000)) 
+ycoords = list(range(1000))
+timesteps = list(range(1,101)) # Number of steps in each walk, integers from 1 to 1000 inclusive
+initial_position = [500,500] #Initial x,y coordinates of spectrin for each walk
+iterations = list(range(1000)) # Number of walks that I want to simulate
 
-thing = plt.hist(stuff, bins = 25)
-plt.xlabel('End to End Distance of Random Walk')
-plt.ylabel('Frequency')
-plt.show(thing)
-
-
-#final = step(initial_position, xcoords, ycoords)
-
-# Call to random walk function
-#walk = rand_walk(timesteps, initial_position, xcoords, ycoords) #function call to random walk, with my input values
-#print(walk)
-
-# Plotting the position of the particle for all time steps with matplotlib
-
-#plt.xlabel("X Coordinate")
+# SCATTER PLOT - Code to visualise a single random walk as a scatter plot for a given number of steps
+#route = walk(initial_position, timesteps)
+#plt.scatter(route[:,0], route[:,1])
+#plt.xlabel('x Coordinate')
+#plt.ylabel('y Coordinate')
 #plt.xlim(0,100)
 #plt.ylim(0,100)
-#plt.ylabel("Y Coordinate")
-#plt.scatter(walk[:,0],walk[:,1])
 #plt.show()
+
+# HISTOGRAM - Code to find mean distance travelled and plot a histogram of distances for a given number of walks
+multiwalks = travels(iterations)
+avg_walk = np.mean(multiwalks)
+print(avg_walk)
+hist_plot = plt.hist(multiwalks, bins = 25)
+plt.xlabel('End to End Distance of Random Walk')
+plt.ylabel('Frequency')
+plt.show(hist_plot)
