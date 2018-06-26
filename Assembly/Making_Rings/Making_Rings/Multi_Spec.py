4ec881a3b0b822556c9133385ad2b4da15e04ca9
YenneV <xhelenmariex@yahoo.com>
2018-06-26 17:04:09 +0100

    First Steps at Multiple Spectrins
    Step function updated to test for occupation status as well as boundaries.  Updates coordinates depending on whether trial move is accepted or rejected.

--- Assembly/Making_Rings/Making_Rings/Multi_Spec.py
+++ Assembly/Making_Rings/Making_Rings/Multi_Spec.py
@@ -1,16 +1,63 @@
-#Monte Carlo Type Simulation for Spectrin Tetramers in a 2D Cartesian Space
+#Monte Carlo Type Simulation for multiple structureless spectrin particles in a 2D Cartesian space
 
-from random import randint
+import numpy as np
+import random
+random.seed(10)
 
-# stepper takes x,y coordinates as input
-# increments randomly in positive/negative x or y
-# then returns the updated coordinates
 
-def stepper(coords):
-    x = coords[0] # Taking initial coordinates and assigning to x and y
-    y = coords[1]
-    step = randint(0,3) # Choosing a random number
-    if step == 0: #moving left, right, up or down depending on number drawn
+# Create some parameters that I can specify
+spectrins = list(range(5)) # Number of particles I'd like - This gives the numbers 0 to 4, 5 numbers in total
+xcoords = list(range(10)) #Length 10
+ycoords = list(range(10)) #Length 10
+
+# Data structures that I will be applying my functions to
+occupation_status = np.zeros([len(xcoords),len(ycoords)], dtype=int)
+coordinates = np.zeros([len(spectrins),2], dtype = int)
+
+# Setting some initial values - will think of a better way of doing this later on
+coordinates[0,:] = [1,1]
+coordinates[1,:] = [1,2]
+coordinates[2,:] = [1,3]
+coordinates[3,:] = [2,1]
+coordinates[4,:] = [2,2]
+
+# I also need to initialise my occupation grid...
+
+
+# Function occupation_check takes a set of xy coordinates and a grid of occupation status
+# and returns true if space is free and false if not
+def occupation_check(new_coords,occupation_grid):
+    x = new_coords[0]
+    y = new_coords[1]
+
+    if occupation_grid[x,y] == 0:
+        return True #Indicates space is free
+    else:
+        return False #Indicates space is occupied
+
+# Next I need a function that just updates the occupation grid once I've approved a move
+def occupation_update(old_coords,new_coords,occupation_grid):
+    old_x = old_coords[0]
+    old_y = old_coords[1]
+    new_x = new_coords[0]
+    new_y = new_coords[1]
+
+    occupation_grid[old_x,old_y] = 0
+    occupation_grid[new_x,new_y] = 1
+    return occupation_grid
+
+# Function for generating a trial move and testing if it's ok based on range, occupation status
+# and initial coordinates
+def multistep(initial, xrange, yrange, occupation_grid):
+    x = initial[0]
+    print('x is initially')
+    print(x)
+    y = initial[1]
+    print('y is initially')
+    print(y)
+
+    step = random.randint(0,3)
+    if step == 0:
         x += 1
     elif step == 1:
         x -= 1
@@ -18,12 +65,21 @@ def stepper(coords):
         y += 1
     elif step == 3:
         y -= 1
+    else: #Not really necessary but just to make sure rand is drawn in range
+        print('Step error - out of range')
 
-    return list([x,y]) # returning updated coordinates after step
-
-#Input coordinates
-coords = [1,2]
-
-#Call to stepper function
-step = stepper(coords)
-print(step)
+    if x not in xrange or y not in yrange:
+        x = initial[0]
+        y = initial[1]
+        print('step rejected - out of range')
+        final = [x,y]
+        return final
+    elif occupation_grid[x,y] == 1:
+        x = initial[0]
+        y = initial[1]
+        print('position already occupied')
+        final = [x,y]
+        return final
+    else:
+        final = [x,y]
+        return final
