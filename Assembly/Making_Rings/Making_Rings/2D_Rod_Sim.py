a4ad5d3b2b356eec24a8e5e90731cc02c3ebbaae
YenneV <xhelenmariex@yahoo.com>
2018-08-10 18:28:12 +0100

    Rigid Rod free motion complete
    Script complete for translation and rotation of rigid rods.

--- Assembly/Making_Rings/Making_Rings/2D_Rod_Sim.py
+++ Assembly/Making_Rings/Making_Rings/2D_Rod_Sim.py
@@ -6,7 +6,7 @@ import numpy as np
 # I'll need to input some variables
 xmax = 100 #Specify the max xcoord of the cell you used
 ymax = 100 #Specify the max ycoord of the cell you used
-steps = list(range(10)) #Specify the number of steps in the walk so I can read in the right number of files
+steps = list(range(1000)) #Specify the number of steps in the walk so I can read in the right number of files
 
 for i in steps:
     if i%100 == 0:
@@ -26,29 +26,7 @@ for i in steps:
             plt.ylabel('y Coordinate')
             plt.grid()
             plt.title('Step = {}'.format(i))
-            plt.show()
+            plt.savefig('D:\Images\RigidRod_Free\image{}.pdf'.format(i), dpi=350)
+            plt.close()
 
-
-
-
-            #print(data)
-            #xcoords = data[:,0]
-            #ycoords = data[:,1]
-            #plt.axis('square')
-            #plt.xlim((0,100))
-            #plt.ylim((0,100))
-            #circle1 = plt.Circle((xcoords[0],ycoords[0]), radius = 10, fc = 'r')
-            #circle2 = plt.Circle((xcoords[1],ycoords[1]), radius = 10, fc = 'g')
-            #circle3 = plt.Circle((xcoords[2],ycoords[2]), radius = 10, fc = 'b')
-            #plt.gca().add_patch(circle1)
-            #plt.gca().add_patch(circle2)
-            #plt.gca().add_patch(circle3)
-            ##plt.scatter(xcoords,ycoords, color = ['red', 'green', 'blue'])
-            #plt.xlabel('x Coordinate')
-            #plt.ylabel('y Coordinate')
-            #plt.grid()
-            #plt.title('Step = {}'.format(i))
-            #plt.savefig('D:\Images\Multi_3_No_Overlap\image{}.pdf'.format(i), dpi=350)
-            #plt.close()
-            #plt.show()
 
--- Assembly/Making_Rings/Making_Rings/rigid_rod.py
+++ Assembly/Making_Rings/Making_Rings/rigid_rod.py
@@ -11,14 +11,16 @@ class Particle:
     instances = []
     def __init__(self, position, length, radius, orientation):
         self.initialposition = position
-        self.position = position #position of A end?
         self.length = length #rounded ends so this is total length-2R
         self.radius = radius
-        # Setting angle of orientation (in radians) and keeping this between limits
-        self.orientation = orientation #I must give this in radians
+        self.initialorientation = orientation #Angle in radians
+        Particle.instances.append(self) #Beware! If I need to delete particles later they won't be removed from this list!
+    def whereami(self, newcoords, orientation):
+        self.position = newcoords
+        self.orientation = orientation
         if self.orientation >= 2*math.pi: #This keeps the angle between 0 and less than 2pi
             self.orientation = self.orientation - 2*math.pi
-        # Finding the end points depending on the angle
+        #Finding the end points depending on the angle
         if math.pi/2 > self.orientation >= 0:
             self.Ax = -(self.length*math.cos(self.orientation))/2  #This should be the amount that we go back in x by to get left pt
             self.Ay = -(self.length*math.sin(self.orientation))/2 #as above for y
@@ -39,14 +41,10 @@ class Particle:
             self.Ay = (self.length*math.sin(self.orientation))/2
             self.Cx = (self.length*math.cos(self.orientation))/2 
             self.Cy = -(self.length*math.sin(self.orientation))/2
-        # Now we've calculated the delta x and delta y component for each end point, we can find coordinates
+        # Locating coordinates of the endpoint A and C based on centre of mass and delta x and y
         self.pointA = [(self.position[0] + self.Ax), (self.position[1] + self.Ay)]
         self.pointC = [(self.position[0] + self.Cx), (self.position[1]+ self.Cy)]
         self.output = self.pointA, self.pointC
-        # All instances of this class are appended to a list
-        Particle.instances.append(self) #Beware! If I need to delete particles later they won't be removed from this list!
-    def position(self, newcoords):
-        self.position = newcoords
     def proximity(self, other): #this is between centres of mass - i may not need this if i use a grid
         deltax = abs(other.position[0] - self.position[0])
         deltay = abs(other.position[1] - self.position[1])
@@ -56,21 +54,24 @@ class Particle:
 # Setting up some parameters for the simulation cell and the number of steps 
 xcoords = list(range(100)) #Length 100
 ycoords = list(range(100)) #Length 100
-steps = list(range(10)) #10000 Steps
+steps = list(range(10000)) #10000 Steps
 #occ_grid = np.zeros((len(xcoords), len(ycoords)), dtype = bool) #grid of 'False' values for occupation
 
-# Initialising a few objects of the class, Particles
+# Initialising a few objects (IN FUTURE I'LL NEED TO AUTOMATE THIS SO WE HAVE MANY PARTICLES)
 rod1 = Particle([50,50], 10, 2, 0)
+rod1.whereami([50,50], 0)
 rod2 = Particle([20,30], 10, 2, (3*math.pi/2))
+rod2.whereami([20,30], (3*math.pi/2))
 rod3 = Particle([10,10], 10, 2, (math.pi/4))
+rod3.whereami([10,10], (math.pi/4))
 
 
 # 'step' function generates a trial move, based on an initial position
-def step(initial):
+def step(initial, angle):
     x = initial[0]
     y = initial[1]
 
-    step = random.randint(0,3)
+    step = random.randint(0,4)
     if step == 0:
         x += 1
     elif step == 1:
@@ -79,30 +80,33 @@ def step(initial):
         y += 1
     elif step == 3:
         y -= 1
+    elif step == 4:
+        angle += (math.pi/4)
     else: #Not really necessary but just to make sure rand is drawn in range
         print('Step error - out of range')
 
-    trial = [x,y]
+    trial = [x,y,angle]
     return trial
 
 # Function takes a particle, a trial move and the cell limits, and returns true if 
 # the move is accepted and false if it is not (because it leaves the cell or gets 
 # too close to another particle
 def validate(mover, trial_move, xrange, yrange):
-    x = trial_move[0]
-    y = trial_move[1]
+    x = trial_move[0] #redundant?
+    y = trial_move[1] #redundant?
+    angle = trial_move[2] #redundant?
     #find the valid range of x values for any move, and same for y, and return as list
     validx = list(range((min(xrange) + mover.radius), (max(xrange) - mover.radius))) # new part 
     validy = list(range((min(yrange) + mover.radius), (max(yrange) - mover.radius))) # new part
-    mover.position = trial_move
+    mover.whereami(trial_move[0:2], trial_move[2])
+
     moverID = (Particle.instances.index(mover)) #Gives me the index in the class instance list for the moving particle
-    iterations = list(range(0,3)) #List of 0,1,2 - think of a better way of doing this
-    distances = np.zeros([1,3], dtype = float) #Array to hold distances of all particles from the mover
-    for j in iterations:
-        distances[:,j] = (Particle.instances[moverID].proximity(Particle.instances[j]))
-    neighbourdist = distances[distances != 0]
-    closest = np.min(neighbourdist)
-    print(closest)
+    #iterations = list(range(0,3)) #List of 0,1,2 - think of a better way of doing this
+    #distances = np.zeros([1,3], dtype = float) #Array to hold distances of all particles from the mover
+    #for j in iterations:
+    #    distances[:,j] = (Particle.instances[moverID].proximity(Particle.instances[j]))
+    #neighbourdist = distances[distances != 0]
+    #closest = np.min(neighbourdist)
     accept = True
     if x not in validx or y not in validy:
         accept = False
@@ -120,14 +124,15 @@ for i in steps:
     print(i)
     mover = (random.choice(Particle.instances)) #Selects a particle to move
     initial_position = mover.position
-    trial_move = step(initial_position)
+    initial_angle = mover.orientation
+    trial_move = step(initial_position, initial_angle) #Returns list of [x,y,orientation]
     accept = validate(mover, trial_move, xcoords, ycoords) # function to return a yes or no
     if accept == True:
-        mover.position = trial_move
+        mover.whereami(trial_move[0:2], trial_move[2])
         print('move allowed to:')
         print(mover.position)
     else:
-        mover.position = initial_position
+        mover.whereami(initial_position, initial_angle)
         print('move not allowed, remains at:')
         print(mover.position)
     if i%10 == 0:
