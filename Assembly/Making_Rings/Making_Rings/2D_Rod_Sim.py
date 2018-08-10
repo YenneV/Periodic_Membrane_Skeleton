6d8791bd7e0da19c9ec2ce81d63298ea438d13ba
YenneV <xhelenmariex@yahoo.com>
2018-08-10 10:12:20 +0100

    Rigid Rod edits
    Added a plotting file for rigid rods.  Did more work on rigid rod script.

---
+++ Assembly/Making_Rings/Making_Rings/2D_Rod_Sim.py
@@ -0,0 +1,37 @@
+# Plotting file for rigid rods
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# I'll need to input some variables
+xmax = 100 #Specify the max xcoord of the cell you used
+ymax = 100 #Specify the max ycoord of the cell you used
+steps = list(range(10)) #Specify the number of steps in the walk so I can read in the right number of files
+
+for i in steps:
+    if i%100 == 0:
+        with open('D:\Code\Assembly\Making_Rings\Making_Rings\Results\Rigid_Rods\coords{}.txt'.format(i), 'r') as p:
+            data = np.genfromtxt(p, dtype = float, delimiter = " ")
+            print(data[1][0])
+            print(data[1])
+            #print(data)
+            #xcoords = data[:,0]
+            #ycoords = data[:,1]
+            #plt.axis('square')
+            #plt.xlim((0,100))
+            #plt.ylim((0,100))
+            #circle1 = plt.Circle((xcoords[0],ycoords[0]), radius = 10, fc = 'r')
+            #circle2 = plt.Circle((xcoords[1],ycoords[1]), radius = 10, fc = 'g')
+            #circle3 = plt.Circle((xcoords[2],ycoords[2]), radius = 10, fc = 'b')
+            #plt.gca().add_patch(circle1)
+            #plt.gca().add_patch(circle2)
+            #plt.gca().add_patch(circle3)
+            ##plt.scatter(xcoords,ycoords, color = ['red', 'green', 'blue'])
+            #plt.xlabel('x Coordinate')
+            #plt.ylabel('y Coordinate')
+            #plt.grid()
+            #plt.title('Step = {}'.format(i))
+            #plt.savefig('D:\Images\Multi_3_No_Overlap\image{}.pdf'.format(i), dpi=350)
+            #plt.close()
+            #plt.show()
+
--- Assembly/Making_Rings/Making_Rings/rigid_rod.py
+++ Assembly/Making_Rings/Making_Rings/rigid_rod.py
@@ -9,22 +9,44 @@ random.seed(10)
 # Creating Class for rod-like objects
 class Particle:
     instances = []
-    def __init__(self, position, length, width):
+    def __init__(self, position, length, radius, orientation):
         self.initialposition = position
-        self.position = position
-        self.length = length
-        self.width = width
-        min_x = self.position[0] - (int(0.5*self.length))
-        max_x = self.position[0] + (int(0.5*self.length))
-        min_y = self.position[1] - (int(0.5*self.width))
-        max_y = self.position[1] + (int(0.5*self.width))
+        self.position = position #position of A end?
+        self.length = length #rounded ends so this is total length-2R
+        self.radius = radius
+        # Setting angle of orientation (in radians) and keeping this between limits
+        self.orientation = orientation #I must give this in radians
+        if self.orientation >= 2*math.pi: #This keeps the angle between 0 and less than 2pi
+            self.orientation = self.orientation - 2*math.pi
+        # Finding the end points depending on the angle
+        if math.pi/2 > self.orientation >= 0:
+            self.Ax = -(self.length*math.cos(self.orientation))/2  #This should be the amount that we go back in x by to get left pt
+            self.Ay = -(self.length*math.sin(self.orientation))/2 #as above for y
+            self.Cx = (self.length*math.cos(self.orientation))/2  #amount we go along in x by to get right pt
+            self.Cy = (self.length*math.sin(self.orientation))/2 #as above for y
+        elif math.pi >= self.orientation >= math.pi/2:
+            self.Ax = (self.length*math.cos(self.orientation))/2  
+            self.Ay = -(self.length*math.sin(self.orientation))/2
+            self.Cx = -(self.length*math.cos(self.orientation))/2 
+            self.Cy = (self.length*math.sin(self.orientation))/2
+        elif (3*math.pi)/2 > self.orientation >= math.pi:
+            self.Ax = (self.length*math.cos(self.orientation))/2  
+            self.Ay = (self.length*math.sin(self.orientation))/2
+            self.Cx = -(self.length*math.cos(self.orientation))/2 
+            self.Cy = -(self.length*math.sin(self.orientation))/2
+        elif (2* math.pi) > self.orientation >= (3*math.pi)/2:
+            self.Ax = -(self.length*math.cos(self.orientation))/2  
+            self.Ay = (self.length*math.sin(self.orientation))/2
+            self.Cx = (self.length*math.cos(self.orientation))/2 
+            self.Cy = -(self.length*math.sin(self.orientation))/2
+        # Now we've calculated the delta x and delta y component for each end point, we can find coordinates
+        self.pointA = [(self.position[0] + self.Ax), (self.position[1] + self.Ay)]
+        self.pointC = [(self.position[0] + self.Cx), (self.position[1]+ self.Cy)]
+        self.output = self.pointA, self.pointC
+        # All instances of this class are appended to a list
         Particle.instances.append(self) #Beware! If I need to delete particles later they won't be removed from this list!
     def position(self, newcoords):
         self.position = newcoords
-        self.min_x = self.position[0] - (int(0.5*self.length))
-        self.max_x = self.position[0] + (int(0.5*self.length))
-        self.min_y = self.position[1] - (int(0.5*self.width))
-        self.max_y = self.position[1] + (int(0.5*self.width))
     def proximity(self, other): #this is between centres of mass - i may not need this if i use a grid
         deltax = abs(other.position[0] - self.position[0])
         deltay = abs(other.position[1] - self.position[1])
@@ -34,34 +56,13 @@ class Particle:
 # Setting up some parameters for the simulation cell and the number of steps 
 xcoords = list(range(100)) #Length 100
 ycoords = list(range(100)) #Length 100
-steps = list(range(10000)) #10000 Steps
-occ_grid = np.zeros((len(xcoords), len(ycoords)), dtype = bool) #grid of 'False' values for occupation
+steps = list(range(10)) #10000 Steps
+#occ_grid = np.zeros((len(xcoords), len(ycoords)), dtype = bool) #grid of 'False' values for occupation
 
 # Initialising a few objects of the class, Particles
-rod1 = Particle([50,50], 10, 2)
-rod2 = Particle([20,30], 10, 2)
-rod3 = Particle([10,10], 10, 2)
-
-# A function to take a particle and a position, calculate the XV and check if grid spaces are free
-def xv_calc(particle, coordinates):
-        
-
-# A function to take a particle and a position and change it's positions to false
-
-# A function to take a particle and a position and change it's position to true
-
-# Initialise grid?
-def grid_check(particle):
-    for i in range(particle.min_x, particle.max_x):
-        for j in range(particle.min_y, particle.max_y):
-            occ_grid[i,j] = True
-    return occ_grid
-
-
-# Need a list of particles to iterate over too...
-for i in range(min_x, max_x):
-    for j in range(min_y, max_y):
-        occ_grid[i,j] = True
+rod1 = Particle([50,50], 10, 2, 0)
+rod2 = Particle([20,30], 10, 2, (3*math.pi/2))
+rod3 = Particle([10,10], 10, 2, (math.pi/4))
 
 
 # 'step' function generates a trial move, based on an initial position
@@ -87,59 +88,72 @@ def step(initial):
 # Function takes a particle, a trial move and the cell limits, and returns true if 
 # the move is accepted and false if it is not (because it leaves the cell or gets 
 # too close to another particle
-#def validate(mover, trial_move, xrange, yrange):
-#    x = trial_move[0]
-#    y = trial_move[1]
-#    #find the valid range of x values for any move, and same for y, and return as list
-#    validx = list(range((min(xrange) + mover.radius), (max(xrange) - mover.radius))) # new part 
-#    validy = list(range((min(yrange) + mover.radius), (max(yrange) - mover.radius))) # new part
-#    mover.position = trial_move
-#    moverID = (Particle.instances.index(mover)) #Gives me the index in the class instance list for the moving particle
-#    iterations = list(range(0,3)) #List of 0,1,2 - think of a better way of doing this
-#    distances = np.zeros([1,3], dtype = float) #Array to hold distances of all particles from the mover
-#    for j in iterations:
-#        distances[:,j] = (Particle.instances[moverID].proximity(Particle.instances[j]))
-#    neighbourdist = distances[distances != 0]
-#    closest = np.min(neighbourdist)
-#    print(closest)
-#    accept = True
-#    if x not in validx or y not in validy:
-#        accept = False
-#        print('step rejected - out of range')
-#    elif closest < mindist:
-#        accept = False
-#        print('step rejected - overlaps another particle')
-#    else:
-#        accept = True
-#    return accept
+def validate(mover, trial_move, xrange, yrange):
+    x = trial_move[0]
+    y = trial_move[1]
+    #find the valid range of x values for any move, and same for y, and return as list
+    validx = list(range((min(xrange) + mover.radius), (max(xrange) - mover.radius))) # new part 
+    validy = list(range((min(yrange) + mover.radius), (max(yrange) - mover.radius))) # new part
+    mover.position = trial_move
+    moverID = (Particle.instances.index(mover)) #Gives me the index in the class instance list for the moving particle
+    iterations = list(range(0,3)) #List of 0,1,2 - think of a better way of doing this
+    distances = np.zeros([1,3], dtype = float) #Array to hold distances of all particles from the mover
+    for j in iterations:
+        distances[:,j] = (Particle.instances[moverID].proximity(Particle.instances[j]))
+    neighbourdist = distances[distances != 0]
+    closest = np.min(neighbourdist)
+    print(closest)
+    accept = True
+    if x not in validx or y not in validy:
+        accept = False
+        print('step rejected - out of range')
+    #elif closest < mindist:
+    #    accept = False
+    #    print('step rejected - overlaps another particle')
+    else:
+        accept = True
+    return accept
 
 # Random Walk - particle chosen at random, coordinates read in, 'step' function called
 # to take a trial step, 'validate' function called to say if ok or not, coordinates updated accordingly
-#for i in steps:
-#    print(i)
-#    mover = (random.choice(Particle.instances)) #Selects a particle to move
-#    initial_position = mover.position
-#    trial_move = step(initial_position)
-#    accept = validate(mover, trial_move, xcoords, ycoords) # function to return a yes or no
-#    if accept == True:
-#        mover.position = trial_move
-#        print('move allowed to:')
-#        print(mover.position)
-#    else:
-#        mover.position = initial_position
-#        print('move not allowed, remains at:')
-#        print(mover.position)
-#    if i%10 == 0:
-#        with open('D:\Code\Assembly\Making_Rings\Making_Rings\Results\Rigid_Rods\coords{}.txt'.format(i), 'w') as f:
-#            f.write(str(spectrin1.position[0]))
-#            f.write(" ")
-#            f.write(str(spectrin1.position[1]))
-#            f.write('\n')
-#            f.write(str(spectrin2.position[0]))
-#            f.write(" ")
-#            f.write(str(spectrin2.position[1]))
-#            f.write('\n')
-#            f.write(str(spectrin3.position[0]))
-#            f.write(" ")
-#            f.write(str(spectrin3.position[1]))
-#            f.write('\n')
+for i in steps:
+    print(i)
+    mover = (random.choice(Particle.instances)) #Selects a particle to move
+    initial_position = mover.position
+    trial_move = step(initial_position)
+    accept = validate(mover, trial_move, xcoords, ycoords) # function to return a yes or no
+    if accept == True:
+        mover.position = trial_move
+        print('move allowed to:')
+        print(mover.position)
+    else:
+        mover.position = initial_position
+        print('move not allowed, remains at:')
+        print(mover.position)
+    if i%10 == 0:
+        with open('D:\Code\Assembly\Making_Rings\Making_Rings\Results\Rigid_Rods\coords{}.txt'.format(i), 'w') as f:
+            f.write(str(rod1.pointA[0]))
+            f.write(" ")
+            f.write(str(rod1.pointA[1]))
+            f.write(" ")
+            f.write(str(rod1.pointC[0]))
+            f.write(" ")
+            f.write(str(rod1.pointC[1]))
+            f.write('\n')
+            f.write(str(rod2.pointA[0]))
+            f.write(" ")
+            f.write(str(rod2.pointA[1]))
+            f.write(" ")
+            f.write(str(rod2.pointC[0]))
+            f.write(" ")
+            f.write(str(rod2.pointC[1]))
+            f.write('\n')
+            f.write(str(rod3.pointA[0]))
+            f.write(" ")
+            f.write(str(rod3.pointA[1]))
+            f.write(" ")
+            f.write(str(rod3.pointC[0]))
+            f.write(" ")
+            f.write(str(rod3.pointC[1]))
+            f.write('\n')
+            
